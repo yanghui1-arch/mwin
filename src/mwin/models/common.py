7e4ea84fdb7c7c3b12d35fd6fe207ed1572fa3c8
dass90 <3053034939@qq.com>
2025-12-29 14:44:46 +0800

    [mwin] fix: fix LLMProvider and add comments

--- src/mwin/models/common.py
+++ src/mwin/models/common.py
@@ -1,9 +1,22 @@
 from enum import Enum
 
 class LLMProvider(Enum):
+    """LLMProvider is AI inference engine.
+    Lots of existing inference engine such as vllm, sglang and so on. Different engine has their own input format and return type.
+    
+    OPENAI: use openai sdk
+    GOOGLE: use google sdk
+    ANTHROPIC: use anthropic sdk
+    OLLAMA: use ollama sdk or its inference server
+    VLLM: use VLLM sdk or its inference server
+    HF: use hugging face seires products or sdk.
+    SGLANG: use sglang sdk
+    """
+    
     OPENAI = 'openai'
     GOOGLE = 'google'
     ANTHROPIC = 'anthropic'
-    DEEPSEEK = 'deepseek'
-    QWEN = 'qwen'
     OLLAMA = 'ollama'
+    VLLM = "vllm"
+    HF = "hugging_face"
+    SGLANG = "sglang"
