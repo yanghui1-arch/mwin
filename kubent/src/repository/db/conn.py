92583355899150b8418b7f2b67536cd78321f381
dass90 <3053034939@qq.com>
2025-12-29 15:38:39 +0800

    [kubent] feat: add sync connection pool

--- kubent/src/repository/db/conn.py
+++ kubent/src/repository/db/conn.py
@@ -1,10 +1,13 @@
 from collections.abc import AsyncGenerator
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
     AsyncSession,
 )
 
+# Async Session
 DATABASE_URL = (
     "postgresql+asyncpg://postgres:123456@localhost:5432/aitrace"
 )
@@ -24,3 +27,19 @@ AsyncSessionLocal = async_sessionmaker(
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
+
+# Sync session. (temp)
+DATABASE_URL_SYNC = "postgresql://postgres:123456@localhost:5432/aitrace"
+engine_sync = create_engine(
+    DATABASE_URL_SYNC,
+    pool_size=10,
+    max_overflow=20,
+    echo=True,
+)
+
+SessionLocal = sessionmaker(
+    bind=engine_sync,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+)
