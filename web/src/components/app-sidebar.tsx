0a61d3c14cde31eed929c52253ae8fcca27228cd
dass90 <3053034939@qq.com>
2026-01-20 16:21:59 +0800

    [web] mwin -> optimizer

--- web/src/components/app-sidebar.tsx
+++ web/src/components/app-sidebar.tsx
@@ -99,7 +99,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="size-5!" />
-                <span className="text-base font-semibold">Mwin Inc.</span>
+                <span className="text-base font-semibold">Optimizer</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
