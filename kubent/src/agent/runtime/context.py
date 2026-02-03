b765f78be4b9cdd788300b68d4f29ab2935bef85
dass90 <3053034939@qq.com>
2026-02-03 23:29:59 +0800

    fix(kubent-context): add default field to contextvar initialization

--- kubent/src/agent/runtime/context.py
+++ kubent/src/agent/runtime/context.py
@@ -4,5 +4,5 @@ from contextvars import ContextVar
 current_session_id = ContextVar[UUID | None]("current_session_id", default=None)
 current_user_id = ContextVar[UUID | None]("current_user_id", default=None)
 current_project_name = ContextVar[UUID | None]("current_project_name", default=None)
-current_agent_name = ContextVar[str]("current_agent_name", "Kubent")
+current_agent_name = ContextVar[str]("current_agent_name", default="Kubent")
 
