d43f6446ed6ac7192152e41dbc21b8e61fbd4f12
dass90 <3053034939@qq.com>
2026-02-06 22:16:30 +0800

    fix(kubent-celery): initialzie sandbox manager at the start of celery

--- kubent/src/kubent_celery/celery_app.py
+++ kubent/src/kubent_celery/celery_app.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.signals import worker_init
 
 celery_app = Celery(
     "worker",
@@ -16,3 +17,10 @@ celery_app.conf.update(
 
 celery_app.set_default()
 celery_app.autodiscover_tasks(["src.kubent_celery.tasks"])
+
+
+@worker_init.connect
+def setup_sandbox(**kwargs):
+    from ..sandbox import init_docker_client, init_sandbox_manager
+    docker_client = init_docker_client()
+    init_sandbox_manager(capacity=64, docker_client=docker_client)
