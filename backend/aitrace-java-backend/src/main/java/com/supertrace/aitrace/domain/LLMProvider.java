4f4a1cef6ef69001d5ff24e2174325376ed27766
dass90 <3053034939@qq.com>
2026-02-21 13:12:45 +0800

    [JAVA-BACKEND] feat: add a function to get name from string of LLMProvider

--- backend/aitrace-java-backend/src/main/java/com/supertrace/aitrace/domain/LLMProvider.java
+++ backend/aitrace-java-backend/src/main/java/com/supertrace/aitrace/domain/LLMProvider.java
@@ -1,5 +1,11 @@
 package com.supertrace.aitrace.domain;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Getter
 public enum LLMProvider {
     OPEN_ROUTER("open_router"),
     DASH_SCOPE("dash_scope"),
@@ -11,7 +17,18 @@ public enum LLMProvider {
     OLLAMA("ollama"),
     TRANSFORMERS("transformers");
 
-    LLMProvider(String description) {
+    private final String value;
+    LLMProvider(String value) {
+        this.value = value;
+    }
+
+    public static LLMProvider fromString(String value) {
+
+        return Optional.ofNullable(value)
+            .map(String::toUpperCase)
+            .filter(s -> Arrays.stream(LLMProvider.values()).anyMatch(e -> e.name().equals(s)))
+            .map(LLMProvider::valueOf)
+            .orElse(null);
     }
 }
 
